0522fbaf195f791e22c00822027d424703f27ef1
sunilkraf <sunil.kumar@roofandfloor.com>
2019-10-13 22:01:39 +0530

    API Testing tutorial added

---
+++ src/com/apiTesting/Main.java
@@ -0,0 +1,25 @@
+package com.apiTesting;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class Main {
+
+    @Test
+    public static void Test() {
+
+        RestAssured.baseURI = "https://maps.googleapis.com";
+
+        given().param("location","-33.8670522,151.1957362").param("radius","1500").
+                param("type","restaurant").param("keyword","cruise").
+                param("key","AIzaSyBF0YL-HA1WEDLyrYwhcLYkTHoGfmEMT5Y").
+                when().get("maps/api/place/nearbysearch/json").
+                then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
+                body("results[0].geometry.location.lat",equalTo("-33.858543")).and().
+                body("results[0].place_id.",equalTo("ChIJi6C1MxquEmsR9-c-3O48ykI")).and().
+                header("Server","scaffolding on HTTPServer2");
+    }
+}
